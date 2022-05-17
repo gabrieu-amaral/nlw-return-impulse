d63991d0b2358429bb4628acf6e46e4104a958fd
Gabriel Amaral <gabriel100j@hotmial.com>
2022-05-17 16:47:50 -0300

    add port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,6 +27,6 @@ app.use(function(req, res, next) {
     'preflightContinue': false
   }));
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server running!');
 });
